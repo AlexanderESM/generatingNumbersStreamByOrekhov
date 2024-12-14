6b52a3c129aaa16b725c57380842ca6b26699990
Alexander O <orekhov-alexander@rambler.ru>
2024-12-14 21:27:23 +0300

    1

--- src/main/java/StreamCollectorsExample.java
+++ src/main/java/StreamCollectorsExample.java
@@ -22,11 +22,20 @@ class Order {
 public class StreamCollectorsExample {
     public static void main(String[] args) {
         List<Order> orders = List.of(
-                new Order("Laptop", 1200.0),
-                new Order("Smartphone", 800.0),
-                new Order("Laptop", 1500.0),
-                new Order("Tablet", 500.0),
-                new Order("Smartphone", 900.0)
+                new Order("Microwave", 8000),
+                new Order("Microwave", 5000),
+                new Order("Fridge", 100000),
+                new Order("Fridge", 50000),
+                new Order("Washing machine", 40000),
+                new Order("Washing machine", 90000),
+                new Order("dishwasher", 40000),
+                new Order("dishwasher", 60000),
+                new Order("Laptop", 180000),
+                new Order("Laptop", 80000),
+                new Order("Tablet", 5000),
+                new Order("Tablet", 12000),
+                new Order("Smartphone", 12000.0),
+                new Order("Smartphone", 20000)
         );
 
         // Группировка заказов по продуктам и вычисление общей стоимости для каждого продукта
@@ -43,7 +52,7 @@ public class StreamCollectorsExample {
                 .collect(Collectors.toList());
 
         // Вывод результата
-        System.out.println("Top 3 most expensive products:");
+        System.out.println("Три самых дорогих продукта:");
         topThreeProducts.forEach(entry ->
                 System.out.println(entry.getKey() + ": " + entry.getValue()));
     }
